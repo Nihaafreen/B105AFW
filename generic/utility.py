30d8aa8063b12c99591c91e8904d363a24fb91e6
Nihaafreen <afreenniha14@gmail.com>
2024-07-09 20:09:49 +0530

    For multiple invalid login TC

--- generic/utility.py
+++ generic/utility.py
@@ -6,7 +6,23 @@ class Excel:
            wb= openpyxl.load_workbook(filepath)
            sheet=wb[sheet_name]
            value=sheet.cell(row,col).value
-      except:
+           if value==None:
+               print("Cell value is NONE")
+               value=""
+      except Exception as e:
+           print(str(e))
            value=""
 
       return value
+
+    @staticmethod
+    def get_rowcount(filepath,sheet_name):
+      try:
+           wb= openpyxl.load_workbook(filepath)
+           sheet=wb[sheet_name]
+           rc=sheet.max_row
+      except Exception as e:
+           print(str(e))
+           rc=0
+
+      return rc
